6c3b0eff3c951305b36ec2b1813123b60c26d9a5
Jorge Espina <canchemolinas@gmail.com>
2023-01-26 18:16:33 -0600

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Ejercicio-2-
+ 
+#  Enunciado
+## Ejercicio2
+ 
+Un programa que indique si la cadena ingresada es un palíndromo (un palíndromo es una palabra,
+número o frase que se lee igual hacia adelante que hacia atrás, por ejemplo: RECONOCER). 
+El programa debe recibe la palabra como parámetro.
+
+
+## Indicaciones
+Solo correrlo y ingresar en consola la palabra o cadena para verificar si es polindromo o no.
+
--- SolveExercise2/src/Main.java
+++ SolveExercise2/src/Main.java
@@ -1,53 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-
+    public static String IsPalindromo(String CadenaIngresada, String CadenaInvertida){
+        if(CadenaIngresada.equals(CadenaInvertida)){
+            return "Es Palindromo";
+        }
+        return "No es Palindromo";
+    }
     public static void main(String[] args) {
         String CadenaIngresada = "";
         String CadenaInvertida = "";
         char Caracter;
         Scanner s = new Scanner(System.in);
-        boolean salir = false;
-        int opcion;
-        while(!salir){
-            System.out.println("1. Ingresar Cadena o Palabra");
-            System.out.println("2. Salir");
+        System.out.println("--------------------------");
 
-            System.out.println("Escribe una de las opciones");
-            opcion = s.nextInt();
-            switch (opcion){
-                case 1:
-                    System.out.println("--------------------------");
-
-                    System.out.println("Ingrese una Cadena u palabra ");
-                    CadenaIngresada = s.next().toLowerCase();
-                    int contador = CadenaIngresada.length()-1;
-                    while ( contador >= 0){
-                        Caracter = CadenaIngresada.charAt(contador);
-                        CadenaInvertida += Caracter;
-                        contador--;
-                    }
-                    if(CadenaIngresada.equals(CadenaInvertida)){
-                        System.out.println("Es Palindromo");
-                    }else{
-                        System.out.println("No es Palindromo");
-                    }
-                    CadenaInvertida = "";
-                    CadenaIngresada = "";
-                    System.out.println("--------------------------");
-                    break;
-                case 2:
-                    salir = true;
-                    break;
-                default:
-                    System.out.println("Solo numeros entre 1 y 2");
-            }
+        System.out.print("Ingrese una Cadena u palabra \n");
+        CadenaIngresada = s.next().toLowerCase();
+        int contador = CadenaIngresada.length()-1;
+        while ( contador >= 0){
+            Caracter = CadenaIngresada.charAt(contador);
+            CadenaInvertida += Caracter;
+            contador--;
         }
-
-
-
-
-
+        System.out.println(IsPalindromo(CadenaIngresada,CadenaInvertida));
+        System.out.println("--------------------------");
     }
-
 }
